165d880d08ad375711396352b314d46b30e2f67c
Дмитрий Деркач <Dmitriy Derkach>
2023-12-16 18:31:29 +0500

    fix

--- electron/frontend/main.js
+++ electron/frontend/main.js
@@ -1,14 +1,11 @@
 import './style.css'
 import cytoscape from 'cytoscape'
 
+let counter = 0
+
 document.addEventListener('DOMContentLoaded', () => {
   const instance = cytoscape({
     container: document.getElementById('app'),
-    elements: [
-      { data: { id: 'a' } },
-      { data: { id: 'b' } },
-      { data: { source: 'a', target: 'b' } },
-    ],
     style: [
       {
         selector: 'node',
@@ -22,6 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
         style: {
           width: 3,
           'line-color': '#ccc',
+          'curve-style': 'bezier',
           'target-arrow-color': '#ccc',
           'target-arrow-shape': 'triangle',
         },
@@ -29,7 +27,28 @@ document.addEventListener('DOMContentLoaded', () => {
     ],
   })
 
-  instance.on("dbltap", (event, params) => {
+  function createNodeEventListener() {
+    instance.on('dbltap', (event) => {
+        if (event.target == instance) {
+          event.cy.add({
+            data: { id: String(counter++) },
+            position: { x: event.position.x, y: event.position.y },
+          })
+        }
+      })
+  }
 
-  })
+  createNodeEventListener()
+
+  function joinNodesEventListener() {
+    let node = null
+    instance.on('dbltap', 'node', (event) => {
+      console.log(selectedNode)
+
+      if (node === null) {
+        node = event.target
+      }
+    })
+  }
+  joinNodesEventListener()
 })
