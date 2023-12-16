213d101dafae4877aa4aca237e92393336d53449
unknown <yaosobolev@gmail.com>
2023-12-16 20:46:20 +0500

    interface

--- electron/frontend/main.js
+++ electron/frontend/main.js
@@ -1,62 +1,63 @@
-import './style.css'
-import cytoscape from 'cytoscape'
+import "./style.css";
+import cytoscape from "cytoscape";
 
-let counter = 0
+let counter = 0;
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const instance = cytoscape({
-    container: document.getElementById('app'),
+    container: document.getElementById("app"),
     style: [
       {
-        selector: 'node',
+        selector: "node",
+
         style: {
-          'background-color': '#666',
-          label: 'data(id)',
+          "background-color": "#666",
+          label: "data(id)",
         },
       },
       {
-        selector: '.selected',
+        selector: ".selected",
         style: {
-          'border-width': 2,
-          'border-color': 'red',
-          'border-style': 'solid',
+          "border-width": 2,
+          "border-color": "red",
+          "border-style": "solid",
         },
       },
       {
-        selector: 'edge',
+        selector: "edge",
         style: {
           width: 3,
-          'line-color': '#ccc',
-          'curve-style': 'bezier',
-          'target-arrow-color': '#ccc',
-          'target-arrow-shape': 'triangle',
+          "line-color": "#ccc",
+          "curve-style": "bezier",
+          "target-arrow-color": "#ccc",
+          "target-arrow-shape": "triangle",
         },
       },
     ],
-  })
+  });
 
   function createNodeEventListener() {
-    instance.on('dbltap', (event) => {
+    instance.on("dbltap", (event) => {
       if (event.target == instance) {
         event.cy.add({
           data: { id: String(counter++) },
           position: { x: event.position.x, y: event.position.y },
-        })
+        });
       }
-    })
+    });
   }
 
-  createNodeEventListener()
+  createNodeEventListener();
 
   function joinNodesEventListener() {
-    let node = null
-    instance.on('dbltap', 'node', (event) => {
+    let node = null;
+    instance.on("dbltap", "node", (event) => {
       if (node === null) {
-        node = event.target
-        node.addClass('selected')
-        return
+        node = event.target;
+        node.addClass("selected");
+        return;
       }
-    })
+    });
   }
-  joinNodesEventListener()
-})
+  joinNodesEventListener();
+});
