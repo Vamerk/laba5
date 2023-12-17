fb49464d129c3b8460a5899b0fd6e9d955c09cde
Дмитрий Деркач <Dmitriy Derkach>
2023-12-17 19:51:35 +0500

    fix

---
+++ electron/backend/index.js
@@ -0,0 +1,23 @@
+const { spawn } = require('child_process')
+
+const cppProcess = spawn('./cpp/test.exe')
+
+// Отправляем данные из Node.js в C++
+const inputData = 'Hello from Node.js!';
+cppProcess.stdin.write(inputData);
+cppProcess.stdin.end();  // Закрываем поток ввода, чтобы C++ программе было понятно, что данные завершены
+
+// Обрабатываем вывод C++ программы
+cppProcess.stdout.on('data', (data) => {
+  console.log(`Получено из C++: ${data}`);
+});
+
+// Обрабатываем ошибки, если они возникнут
+cppProcess.stderr.on('data', (data) => {
+  console.error(`Ошибка из C++: ${data}`);
+});
+
+// Обрабатываем завершение C++ программы
+cppProcess.on('close', (code) => {
+  console.log(`C++ программа завершилась с кодом ${code}`);
+});
